3dc8a2a24df9da865c483a14625ed444bf04e577
MrDoritos <ianmoore322@gmail.com>
2020-05-17 15:08:26 -0600

    nothing

--- console.tests.linux.sh
+++ console.tests.linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-g++ console.linux.cpp console.tests.cpp -I. -lncurses -o console.tests.linux $@ 2> out.txt
+g++ console.linux.cpp console.tests.cpp -I. -lncursesw -o console.tests.linux $@ 2> out.txt
 if [[ $? != 0 ]]
 	then
 	less out.txt
