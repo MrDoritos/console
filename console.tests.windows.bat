6673e5bb7dddf482b2d68b008a60b7172657916b
MrDoritos <ianmoore322@gmail.com>
2020-05-17 14:19:57 -0600

    It just werks

--- console.tests.windows.bat
+++ console.tests.windows.bat
@@ -1,9 +1,9 @@
 @echo off
 
-g++ console.windows.cpp console.tests.cpp -I. -o console.tests.windows.exe %* > out.txt
+cmd /c g++ console.windows.cpp console.tests.cpp -I. -o console.tests.windows.exe %*
 
 if "%errorlevel%" NEQ "0" (
-	more out.txt
+	pause
 	) else (
 	echo console.tests.cpp -\> console.tests.windows.exe
 )
